f67f4fc590b64b16d1905e92c30cc9dc959614bc
Juan Sutil <jsutmes2712@g.educaand.es>
2023-11-14 12:51:14 +0100

    m

--- huelgasMongo/src/main/java/es/iesjandula/huelgasMongo/connections/MongoClientConnection.java
+++ huelgasMongo/src/main/java/es/iesjandula/huelgasMongo/connections/MongoClientConnection.java
@@ -2,6 +2,7 @@ package es.iesjandula.huelgasMongo.connections;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.bson.Document;
@@ -18,11 +19,14 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
+import es.iesjandula.huelgasMongo.utils.Alumno;
+
 public class MongoClientConnection
 {
-	
+
 	private MongoDatabase database;
 	private MongoClient mongoClient;
+
 	/**
 	 * @param connectionString
 	 */
@@ -30,60 +34,57 @@ public class MongoClientConnection
 	{
 		ServerApi serverApi = ServerApi.builder().version(ServerApiVersion.V1).build();
 
-		MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(new ConnectionString(connectionString)).serverApi(serverApi).build();
-		
+		MongoClientSettings settings = MongoClientSettings.builder()
+				.applyConnectionString(new ConnectionString(connectionString)).serverApi(serverApi).build();
+
 		this.mongoClient = MongoClients.create(settings);
-		
+
 		try
 		{
-			this.database = this.mongoClient.getDatabase("InfoHuelgas");
+			this.database = this.mongoClient.getDatabase("HuelgaMongo");
 		} catch (MongoException e)
 		{
 			System.exit(0);
 		}
 	}
-	
-	public void closeDatabase() {
+
+	public void closeDatabase()
+	{
 		this.mongoClient.close();
 	}
-	
-	public MongoDatabase getDatabase() {
+
+	public MongoDatabase getDatabase()
+	{
 		return this.database;
 	}
-	
-	 public String findDocument(String collection, String key, String value) {
-	    // Select the collection
-	    MongoCollection<Document> col = this.database.getCollection(collection);
 
-	    // Create the document to specify find criteria
-	    Document findDocument = new Document(key, value);
+	public String findDocument(String collection, String key, String value)
+	{
+		// Select the collection
+		MongoCollection<Document> col = this.database.getCollection(collection);
 
-	    // Document to store query results
-	    FindIterable<Document> resultDocument = col.find(findDocument);
+		// Create the document to specify find criteria
+		Document findDocument = new Document(key, value);
 
-	    // Return the name of the first returned document
-	    return resultDocument.first().toJson();
+		// Document to store query results
+		FindIterable<Document> resultDocument = col.find(findDocument);
 
-	  }
-	 
-	 public void showPeopleByEmail(String curso) {
-		    // Select the "people" collection
-	    MongoCollection<Document> collection = this.database.getCollection("people");
-	    
-	    Map<String, String> parameters = new HashMap<String, String>();
-	    
-	    parameters.put("curso", curso);
-	    // Create the document to specify find criteria
-	    Document findDocument = new Document(parameters);
+		// Return the name of the first returned document
+		return resultDocument.first().toJson();
 
-	    // Document to store query results
-	    MongoCursor<Document> resultDocument = collection.find(findDocument).iterator();
+	}
 
-	    System.out.println("*** Listing People By Email: " + curso);
-	    
-	    // Iterate over the results printing each document
-	    while (resultDocument.hasNext()) {
-	      System.out.println(resultDocument.next().getString("name"));
-	    }
-	  }
+	public void uploadAlumnos(List<Alumno> alumnos, String fecha)
+	{
+		// Select the "people" collection
+		MongoCollection<Document> collection = this.database.getCollection("huelga");
+
+		for (Alumno alumno : alumnos)
+		{
+			Document document = new Document("dni", alumno.getDni()).append("nombre", alumno.getNombre())
+					.append("curso", alumno.getCurso()).append("fecha", fecha);
+			collection.insertOne(document);
+		}
+
+	}
 }
--- huelgasMongo/src/main/java/es/iesjandula/huelgasMongo/launcher/StartWindow.java
+++ huelgasMongo/src/main/java/es/iesjandula/huelgasMongo/launcher/StartWindow.java
@@ -1,10 +1,13 @@
 package es.iesjandula.huelgasMongo.launcher;
 
 import java.awt.EventQueue;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 
 import es.iesjandula.huelgasMongo.connections.MongoClientConnection;
+import es.iesjandula.huelgasMongo.utils.Alumno;
 import es.iesjandula.huelgasMongo.windows.InitialWindow;
 
 
@@ -22,7 +25,10 @@ public class StartWindow
 			{
 				try
 				{
-					MongoClientConnection mongo = new MongoClientConnection("mongodb+srv://admin:admin@bananasl.vvxrhe2.mongodb.net/?retryWrites=true&w=majority");
+					MongoClientConnection mongo = new MongoClientConnection("mongodb+srv://admin:admin@cluster0.0j45czf.mongodb.net/?retryWrites=true&w=majority");
+					List<Alumno> alumnos = new ArrayList<Alumno>();
+					alumnos.add(new Alumno("Juan Sutil", "123456789A", "2 DAM"));
+					mongo.uploadAlumnos(alumnos, "2023-11-16");
 					JFrame initialWindow = new InitialWindow(mongo);
 					initialWindow.setVisible(true);
 				} catch (Exception e)
